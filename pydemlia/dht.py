62da0628290b642b69fcb344f2484cdaa200062e
Edward Pasenidis <40323691+pasenidis@users.noreply.github.com>
2024-11-19 00:07:31 +0200

    feat: send_rpc, put, get, bootstrap funcs in DHT

--- pydemlia/dht.py
+++ pydemlia/dht.py
@@ -1,19 +1,40 @@
+import socket
+import pickle
+
 class DHT:
     def __init__(self, node):
         self.node = node
 
+    def send_rpc(self, target_node, message):
+        serialized_message = pickle.dumps(message)
+        # Assuming target_node is a tuple (IP, port)
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+            s.sendto(serialized_message, (target_node['ip'], target_node['port']))
+
     def put(self, key, value):
         closest_nodes = self.node.routing_table.find_closest_nodes(key)
         for node in closest_nodes:
-            # Send STORE message to each node
-            pass
+            message = {
+                'operation': 'STORE',
+                'key': key,
+                'value': value
+            }
+            self.send_rpc(node, message)
 
     def get(self, key):
         closest_nodes = self.node.routing_table.find_closest_nodes(key)
         for node in closest_nodes:
-            # Send FIND_VALUE request and handle response
-            pass
+            message = {
+                'operation': 'FIND_VALUE',
+                'key': key
+            }
+            self.send_rpc(node, message)
+            # Assume there's some response handling to process the retrieved value
 
     def bootstrap(self, bootstrap_node):
-        # Send FIND_NODE request to boostrap_node
-        pass
+        message = {
+            'operation': 'FIND_NODE',
+            'key': self.node.id
+        }
+        self.send_rpc(bootstrap_node, message)
+        # Assuming the response from bootstrap_node contains the closest nodes to join the network
